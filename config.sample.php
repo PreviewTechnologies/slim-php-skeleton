63b8123b764beefa1a5eebd3c89491ce6c574111
Shaharia Azam <shaharia.azam@gmail.com>
2017-03-24 01:13:02 +0600

    Initial file structure changed

--- config.sample.php
+++ config.sample.php
@@ -1,2 +1,44 @@
 <?php
-return [];
+return [
+    'app' => [
+        'determineRouteBeforeAppMiddleware' => true,
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => ROOT_DIR . DIRECTORY_SEPARATOR . 'templates/',
+        ],
+        // View settings
+        'view' => [
+            'template_path' => ROOT_DIR . DIRECTORY_SEPARATOR . 'templates/',
+            'twig' => [
+                'cache' => ROOT_DIR . DIRECTORY_SEPARATOR . "tmp/cache/twig",
+                'debug' => true,
+                'auto_reload' => true,
+            ],
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'previewtechs-accounts-portal',
+            'path' => ROOT_DIR . DIRECTORY_SEPARATOR . 'logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+            'monolog_handlers' => ['php://stdout', 'file']
+        ],
+
+        'databases' => [
+            'default' => [
+                'driver' => 'mysql',
+                'host' => 'HOST_NAME',
+                'database' => 'NAME_OF_DATABASE',
+                'username' => 'DATABASE_USERNAME',
+                'password' => 'DATABASE_PASSWORD',
+                'charset' => 'Utf8',
+                'collation' => 'utf8_general_ci',
+                'prefix' => '',
+                'unix_socket' => null
+            ]
+        ]
+    ]
+];
